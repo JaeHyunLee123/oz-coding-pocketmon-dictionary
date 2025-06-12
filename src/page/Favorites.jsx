d6b61638d2ce59c24c6d465dbcc1a2b28d4a34e5
JaehyunLee <ljh@JaehyunLees-MacBook-Pro.local>
2025-06-12 16:43:56 +0900

    feat: favorite page

--- src/page/Favorites.jsx
+++ src/page/Favorites.jsx
@@ -1,3 +1,35 @@
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import PocketmonCard from "../components/PocketmonCard";
+
 export default function Favorites() {
-  return <div>Favorites</div>;
+  const [favoritePocketmons, setFavoritePocketmons] = useState([]);
+
+  /** @type {import("../type").PocketmonStore} */
+  const { isLoading, isError, pocketmons } = useSelector(
+    (state) => state.pocketmon
+  );
+
+  /** @type {Array<number>} */
+  const favorites = useSelector((state) => state.favorites);
+
+  useEffect(() => {
+    const tempFavoritePocketmons = pocketmons.filter((pocketmon) =>
+      favorites.includes(pocketmon.id)
+    );
+
+    setFavoritePocketmons(tempFavoritePocketmons);
+  }, [favorites, pocketmons]);
+
+  return (
+    <div className="flex justify-center flex-wrap gap-5">
+      {isLoading
+        ? "Loading..."
+        : isError
+        ? "Error try again"
+        : favoritePocketmons.map((pocketmon) => (
+            <PocketmonCard pocketmon={pocketmon} key={pocketmon.id} />
+          ))}
+    </div>
+  );
 }
