baa5c04f2fe96437f80a08cfe5a463d8f73b3cd1
JaehyunLee <ljh@JaehyunLees-MacBook-Pro.local>
2025-06-12 13:38:27 +0900

    feat: set Routes Main, Detail, Search, Favorites

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { fetchPocketmonsThunk } from "./RTK/pocketmonSlice";
+import { Route, Routes } from "react-router";
+import Main from "./page/Main";
+import Detail from "./page/Detail";
+import Favorites from "./page/Favorites";
+import Search from "./page/Search";
 
 function App() {
   const dispatch = useDispatch();
@@ -8,7 +13,14 @@ function App() {
     dispatch(fetchPocketmonsThunk(151));
   }, [dispatch]);
 
-  return <></>;
+  return (
+    <Routes>
+      <Route path="/" element={<Main />} />
+      <Route path="/detail" element={<Detail />} />
+      <Route path="/search" element={<Search />} />
+      <Route path="/favorites" element={<Favorites />} />
+    </Routes>
+  );
 }
 
 export default App;
