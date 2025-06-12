c82befabad60947dac5d082c86ccd6a6ba47f965
JaehyunLee <ljh@JaehyunLees-MacBook-Pro.local>
2025-06-12 14:52:49 +0900

    feat: add favorite to home

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,12 +1,18 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router";
 import Heart from "../components/Heart";
+import { toggleFavorite } from "../RTK/favoriteSlice";
 
 export default function Home() {
   /** @type {import("../type").PocketmonStore} */
   const { isLoading, isError, pocketmons } = useSelector(
     (state) => state.pocketmon
   );
+
+  /** @type {Array<number>} */
+  const favorites = useSelector((state) => state.favorites);
+
+  const dispatch = useDispatch();
 
   return (
     <div className="flex justify-center flex-wrap gap-5">
@@ -15,11 +21,24 @@ export default function Home() {
         : isError
         ? "Error try again"
         : pocketmons.map((pocketmon) => (
-            <Link to={`/detail/${pocketmon.id}`} key={pocketmon.id}>
+            <div
+              className="flex flex-col justify-center items-center"
+              key={pocketmon.id}
+            >
               <img src={pocketmon.frontImage} />
-              <span>{pocketmon.name}</span>
-              <Heart />
-            </Link>
+              <div className="flex justify-center items-center gap-2">
+                <span>{pocketmon.name}</span>
+                <button
+                  onClick={() => {
+                    dispatch(toggleFavorite(pocketmon.id));
+                  }}
+                >
+                  <Heart
+                    fill={favorites.includes(pocketmon.id) ? "red" : "none"}
+                  />
+                </button>
+              </div>
+            </div>
           ))}
     </div>
   );
