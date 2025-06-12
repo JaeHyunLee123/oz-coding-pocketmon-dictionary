af0ebdc1060ae4b11f28d2121b1a8bff6b80e44d
JaehyunLee <ljh@JaehyunLees-MacBook-Pro.local>
2025-06-12 13:48:04 +0900

    fix: rename Main component to Home Component

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { fetchPocketmonsThunk } from "./RTK/pocketmonSlice";
 import { Route, Routes } from "react-router";
-import Main from "./page/Main";
+import Home from "./page/Home";
 import Detail from "./page/Detail";
 import Favorites from "./page/Favorites";
 import Search from "./page/Search";
@@ -15,7 +15,7 @@ function App() {
 
   return (
     <Routes>
-      <Route path="/" element={<Main />} />
+      <Route path="/" element={<Home />} />
       <Route path="/detail" element={<Detail />} />
       <Route path="/search/:id" element={<Search />} />
       <Route path="/favorites" element={<Favorites />} />
