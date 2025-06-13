4ba1eb15e0ddbc80ab17af6e9658930853785c71
JaehyunLee <ljh@JaehyunLees-MacBook-Pro.local>
2025-06-13 13:19:02 +0900

    feat: add image Loading state:

--- src/components/PocketmonCard.jsx
+++ src/components/PocketmonCard.jsx
@@ -7,11 +7,14 @@ import { useDispatch, useSelector } from "react-redux";
 import { toggleFavorite } from "../RTK/favoriteSlice";
 import Heart from "./Heart";
 import { Link } from "react-router";
+import { useState } from "react";
 
 /**
  * @param {PocketmonCardProps} props
  */
 export default function PocketmonCard({ pocketmon }) {
+  const [isImageLoading, setIsImageLoading] = useState(true);
+
   /** @type {Array<number>} */
   const favorites = useSelector((state) => state.favorites);
 
@@ -23,7 +26,15 @@ export default function PocketmonCard({ pocketmon }) {
       key={pocketmon.id}
     >
       <Link to={`/detail/${pocketmon.id}`}>
-        <img src={pocketmon.frontImage} />
+        <img
+          src={pocketmon.frontImage}
+          alt={`${pocketmon.name}-image`}
+          onLoad={() => {
+            setIsImageLoading(false);
+          }}
+        />
+
+        {isImageLoading ? <span className="text-xl">이미지 로딩중</span> : null}
       </Link>
 
       <div className="flex justify-center items-center gap-2">
