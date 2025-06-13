6d5bff29e087064a0280fc13b6060ca1b7ef2658
JaehyunLee <ljh@JaehyunLees-MacBook-Pro.local>
2025-06-13 11:14:04 +0900

    feat: search input

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,29 @@
-import { Link } from "react-router";
+import { Link, useNavigate } from "react-router";
+import MagnifyingGlass from "./MagnifyingGlass";
 
 export default function Header() {
+  const navigate = useNavigate();
+
+  const onSearchInputChange = (event) => {
+    const searchTerm = event.target.value;
+
+    navigate(`search?qeury=${searchTerm}`);
+  };
+
   return (
     <header className="p-10 flex justify-center items-center gap-2">
       <Link to="/">Home</Link>
       <Link to="/favorites">Favorite</Link>
-      <Link to="/search">Search</Link>
+      <div className="flex justify-center items-center gap-2">
+        <label htmlFor="search">
+          <MagnifyingGlass />
+        </label>
+        <input
+          id="search"
+          onChange={onSearchInputChange}
+          className="border border-black"
+        />
+      </div>
     </header>
   );
 }
