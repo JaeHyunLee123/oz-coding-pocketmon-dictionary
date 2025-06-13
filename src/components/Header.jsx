a033faef69916a878a8a78394ed0fc55979711ac
JaehyunLee <ljh@JaehyunLees-MacBook-Pro.local>
2025-06-13 13:49:47 +0900

    feat: Header style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,18 +12,24 @@ export default function Header() {
   };
 
   return (
-    <header className="p-10 flex justify-center items-center gap-2">
-      <Link to="/">Home</Link>
-      <Link to="/favorites">Favorite</Link>
-      <div className="flex justify-center items-center gap-2">
-        <label htmlFor="search">
-          <MagnifyingGlass />
-        </label>
-        <input
-          id="search"
-          onChange={onSearchInputChange}
-          className="border border-black"
-        />
+    <header className="flex flex-col">
+      <div className="h-12 bg-[#ff0000]" />
+      <div className="flex justify-center items-center bg-black p-2">
+        <h1 className="text-center text-white text-4xl">포켓몬 도감</h1>
+      </div>
+      <div className="flex justify-center items-center p-2 gap-2">
+        <Link to="/">메인페이지</Link>
+        <Link to="/favorites">찜목록</Link>
+        <div className="flex justify-center items-center gap-1">
+          <label htmlFor="search">
+            <MagnifyingGlass />
+          </label>
+          <input
+            id="search"
+            onChange={onSearchInputChange}
+            className="border-b border-b-black focus: outline-0"
+          />
+        </div>
       </div>
     </header>
   );
