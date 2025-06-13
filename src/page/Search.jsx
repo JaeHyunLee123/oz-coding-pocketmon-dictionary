be545d095b1e28296b874f55a4b639e34b8a1e42
JaehyunLee <ljh@JaehyunLees-MacBook-Pro.local>
2025-06-13 11:41:47 +0900

    feat: search page

--- src/page/Search.jsx
+++ src/page/Search.jsx
@@ -1,3 +1,48 @@
+import { getRegExp } from "korean-regexp";
+import { useEffect, useMemo, useState } from "react";
+import { useSearchParams } from "react-router";
+import PocketmonCard from "../components/PocketmonCard";
+import { useSelector } from "react-redux";
+
 export default function Search() {
-  return <div>Search</div>;
+  const searchParam = useSearchParams()[0].get("query");
+
+  const [searchingPocketmon, setSearchingPocketmon] = useState([]);
+
+  const regex = useMemo(
+    () =>
+      getRegExp(searchParam, {
+        initialSearch: true,
+        startsWith: true,
+        endsWith: false,
+        ignoreSpace: true,
+        ignoreCase: true,
+        global: true,
+      }),
+    [searchParam]
+  );
+
+  /** @type {import("../type").PocketmonStore} */
+  const { isLoading, isError, pocketmons } = useSelector(
+    (state) => state.pocketmon
+  );
+
+  useEffect(() => {
+    const temp = pocketmons.filter((pocketmon) => regex.test(pocketmon.name));
+    setSearchingPocketmon(temp);
+  }, [pocketmons, regex, setSearchingPocketmon]);
+
+  return (
+    <div>
+      <div className="flex justify-center flex-wrap gap-5">
+        {isLoading
+          ? "Loading..."
+          : isError
+          ? "Error try again"
+          : searchingPocketmon.map((pocketmon) => (
+              <PocketmonCard pocketmon={pocketmon} key={pocketmon.id} />
+            ))}
+      </div>
+    </div>
+  );
 }
